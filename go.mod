5919a45ebd42a70e7741a40b428903efffd7d010
xavier <morexavierlin@outlook.com>
2019-09-19 17:34:12 +0800

    初始化rpc服务端

--- go.mod
+++ go.mod
@@ -4,6 +4,11 @@ go 1.12
 
 require (
 	github.com/go-redis/redis v6.15.5+incompatible
+	github.com/golang/protobuf v1.2.0
 	github.com/onsi/ginkgo v1.10.1 // indirect
 	github.com/onsi/gomega v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/grpc v0.0.0-00010101000000-000000000000
 )
+
+replace google.golang.org/grpc => github.com/grpc/grpc-go v1.23.1
--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"context"
+	"github.com/lovepluskaka/taskcron/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+type TaskService struct {
+}
+
+func (t TaskService) Create(ctx context.Context, in *proto.CreateRequest) (*proto.CreateResponse, error) {
+	return nil, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8086")
+	if err != nil {
+		log.Fatalf(err.Error())
+		return
+	}
+
+	s := grpc.NewServer()
+	proto.RegisterTaskServiceServer(s, TaskService{})
+	s.Serve(lis)
 }
